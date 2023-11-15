378204ddd3b5348479fcda3dbc4703005fea217f
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-15 00:32:55 -0800

    test

--- input.c
+++ input.c
@@ -1,23 +1,26 @@
-
 #include "main.h"
 
 /**
  * read_line - Reads a line of input from stdin
  * Return: The line from stdin
  */
-char *read_line(void) {
-    char *line = NULL;
-    size_t bufsize = 0;  /* Changed ssize_t to size_t */
+char *read_line(void)
+{
+	char *line = NULL;
+	size_t bufsize = 0;
 
-    if (getline(&line, &bufsize, stdin) == -1) {
-        if (feof(stdin)) {
-            /* We received an EOF */
-            exit(EXIT_SUCCESS);
-        } else  {
-            perror("readline");
-            exit(EXIT_FAILURE);
-        }
-    }
+	if (getline(&line, &bufsize, stdin) == -1)
+	{
+		if (feof(stdin))
+		{
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			perror("readline");
+			exit(EXIT_FAILURE);
+		}
+	}
 
-    return line;
+	return (line);
 }
