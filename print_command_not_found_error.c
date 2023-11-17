1f629d29de2ae8684a0f8f598d296d47b39cd6f7
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2023-11-17 03:29:13 -0800

    test

--- print_command_not_found_error.c
+++ print_command_not_found_error.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * print_command_not_found_error - Prints
- * an error message for a failed command.
- * @command: Command that was submitted by the user.
- * @filename: Name of the file being run (unused).
+ * print_command_not_found_error - Prints an error message for a failed command
+ * @command: Command that was submitted by the user
+ * @filename: Name of the file being run (in this case, the shell executable)
  */
 void print_command_not_found_error(char *command, char *filename)
 {
 	char *error_message = NULL;
-	char *error = ": command not found\n";
+	char *error_format = "%s: 1: %s: not found\n";
 	size_t num_chars;
 
-	(void)filename;
+	num_chars = string_length(error_format) + string_length(command) + 
+				string_length(filename) - 4;
 
-	num_chars = string_length(error) + string_length(command) + 1;
 	error_message = malloc(sizeof(char) * num_chars);
 
 	if (error_message)
 	{
-		snprintf(error_message, num_chars, "%s%s", command, error);
-		write(STDERR_FILENO, error_message, num_chars - 1);
+		snprintf(error_message, num_chars, error_format, filename, command);
+		write(STDERR_FILENO, error_message, string_length(error_message));
 		free(error_message);
 	}
 }
-
